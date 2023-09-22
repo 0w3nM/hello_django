a23a52e03b242d309f896dbc800b88dfa2cef1d5
0w3nM <>
2023-09-22 12:18:57 +0000

    Updated 'Secret Key spelling.

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.eviron.get(
+SECRET_KEY = os.environ.get(
     'SECRET_KEY', 'django-insecure-oxghy#*_xp3e!opm+45^qik*jt=gt-@rzxi3)k(_tp^f89mopt')
 
 # SECURITY WARNING: don't run with debug turned on in production!
