d7873ff49bc2e273de5461c666f96d0b4e4e261b
0w3nM <>
2023-09-22 12:30:28 +0000

    Updated 'Allowed Hosts'.

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get(
+    '8000-0w3nm-hello-django-u4nbk57t34.us2.codeanyapp.com')]
 
 
 # Application definition
